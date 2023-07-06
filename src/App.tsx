ac51ccc44bd9a89e820ae0e96aea02a10a127c46
Cc <2744166358@qq.com>
2023-07-06 22:16:02 +0800

    feat: 🎸 CSS Modules结合less

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useMutation } from '@apollo/client'
 import { UPDATE } from './graphql/demo'
-import './App.css'
 import { Button, Form, Input, ImageUploader } from 'antd-mobile'
 import { useUploadOSS } from './hooks/useUploadOSS'
+
+import styles from './App.module.less';
+import classNames from 'classnames';
 
 
 const App = () => {
@@ -23,37 +25,39 @@ const App = () => {
     );
   };
   return (
-    <Form
-    layout="horizontal"
-    onFinish={onClickHandler}
-    footer={(
-      <Button block type="submit" color="primary" size="large">
-        提交
-      </Button>
-  )}
-  >
-    <Form.Item
-      name="name"
-      label="姓名"
-    >
-      <Input />
-    </Form.Item>
+    <div className={styles.container }>
+        <Form
+          className={classNames(styles.form, styles.formPadding)}
+          layout="horizontal"
+          onFinish={onClickHandler}
+          footer={(
+            <Button block type="submit" color="primary" size="large">
+              提交
+            </Button>
+        )}
+        >
+        <Form.Item
+          name="name"
+          label="姓名"
+        >
+          <Input />
+        </Form.Item>
 
-    <Form.Item
-      name="desc"
-      label="描述"
-    >
-      <Input />
-    </Form.Item>
-    <Form.Item
-      name="avatar"
-      label="头像"
-    >
-      {/* 上传后悔返回一个url，显示对应的头像 */}
-      <ImageUploader upload={uploadHandler} />
-    </Form.Item>
-
-  </Form>
+        <Form.Item
+          name="desc"
+          label="描述"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="avatar"
+          label="头像"
+        >
+          {/* 上传后悔返回一个url，显示对应的头像 */}
+          <ImageUploader upload={uploadHandler} />
+        </Form.Item>
+      </Form> 
+    </div>
   )
 }
 
